87e0a652d43cca21791cbc4ec9119f9a53221f96
seesealonely <seesealonely@gmail.com>
2020-05-02 23:31:59 +0800

    input

--- l.sh
+++ l.sh
@@ -1,7 +1,7 @@
 #this is script for reducing duplicate operate easily
 
 clear
-echo "提示信息"
+echo "input title"
 read s
 quesionname="$(echo -e "${s}" | tr -d '[ ]')"
 echo $quesionname.h
@@ -28,27 +28,27 @@ echo $fenjie>>$quesionname.h
 echo $fenjie>>$quesionname.cc
 
 classname=$quesionname
-cinclude="#include"${classname#*.}.h""
+cinclude="#include\"$classname.h\""
 maintext="int main() \n
 {\n
 \t	Solution s;\n
 \t	s.${classname#*.}();\n
 \t	return 0;\n
 }"
-echo $quesionname>>$quesionname.cc
+echo -e $cinclude>>$quesionname.cc
 echo -e $maintext >> $quesionname.cc
 unset cinclude
 cinclude="#include\"head.h\""
 	echo $cinclude >>$quesionname.h
 	echo $line >>$quesionname.h
 while read line;do
-	echo $line >>$quesionname.h
+	if [ "$line" != '};' ]; then
+	echo -e $line >>$quesionname.h
+	else 
+		break
+	fi
 done
+	echo -e $line >>$quesionname.h
 
-ms
-read makefile>ms
-echo $ms
-unset cinclude
-cinclude=$quesionname
-echo ${cinclude#*.} >>makefile
+echo -e $quesionname >> makefile
 echo "end input"
