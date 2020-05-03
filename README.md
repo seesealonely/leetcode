050511ae4a421a2321f7c68768d91739578229d6
seesealonely <seesealonely@gmail.com>
2020-05-03 19:20:33 +0800

    r

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # leetcode
 personal project about leetcode
 [933.NumberofRecentCalls](https://leetcode.com/problems/number-of-recent-calls/)
-
+[1413.MinimumValuetoGetPositiveStepbyStepSum](https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/)
--- l.sh
+++ l.sh
@@ -5,6 +5,7 @@ echo "input title"
 read s
 quesionname="$(echo -e "${s}" | tr -d '[ ]')"
 urltail="$(echo -e "${s#*.}" | tr 'A-Z' 'a-z' | tr ' ' '-')"
+urltail=${urltail#*-}
 echo $quesionname.h
 touch $quesionname.h 
 echo $quesionname.cc
@@ -28,31 +29,40 @@ fenjie='*/'
 echo $fenjie>>$quesionname.h
 echo $fenjie>>$quesionname.cc
 
-classname=$quesionname
-cinclude="#include\"$classname.h\""
-maintext="int main() \n
-{\n
-\t	Solution s;\n
-\t	s.${classname#*.}();\n
-\t	return 0;\n
-}"
-echo -e $cinclude>>$quesionname.cc
-echo -e $maintext >> $quesionname.cc
+
 unset cinclude
 cinclude="#include\"head.h\""
 	echo $cinclude >>$quesionname.h
 	echo $line >>$quesionname.h
+
+num=1
 while read line;do
+	if [ $num -eq 3 ] ;then 
+		funcname=${line#* }
+		funcname=${funcname% *}
+	fi
 	if [ "$line" != '};' ]; then
 	echo -e $line >>$quesionname.h
 	else 
 		break
 	fi
+	num=$(($num+1))
 done
 	echo -e $line >>$quesionname.h
 
+unset cinclude
+classname=$quesionname
+cinclude="#include\"$classname.h\""
+maintext="int main() \n
+{\n
+\t	Solution s;\n
+\t	s.${funcname}();\n
+\t	return 0;\n
+}"
+echo -e $cinclude>>$quesionname.cc
+echo -e $maintext >> $quesionname.cc
 echo -e $quesionname >> makefile
 echo "end input"
 
 url="https://leetcode.com/problems/"$urltail'/'
-echo -e [$quesionname]("$url")>>README.md
+echo -e "[$quesionname]($url)">>README.md
