08aa47a75d5db2d5b2d1630a54a2c385b42918cd
seesea <seesealonely@gmail.com>
2021-08-03 08:22:24 +0800

    sliding window + dp

---
+++ leetcode/c++/1871.JumpGameVII.cc
@@ -0,0 +1,41 @@
+/*
+You are given a 0-indexed binary string s and two integers minJump and maxJump. In the beginning, you are standing at index 0, which is equal to '0'. You can move from index i to index j if the following conditions are fulfilled:
+
+i + minJump <= j <= min(i + maxJump, s.length - 1), and
+s[j] == '0'.
+
+Return true if you can reach index s.length - 1 in s, or false otherwise.
+
+
+
+Example 1:
+
+Input: s = "011010", minJump = 2, maxJump = 3
+Output: true
+Explanation:
+In the first step, move from index 0 to index 3.
+In the second step, move from index 3 to index 5.
+
+Example 2:
+
+Input: s = "01101110", minJump = 2, maxJump = 3
+Output: false
+
+
+
+Constraints:
+
+2 <= s.length <= 105
+s[i] is either '0' or '1'.
+s[0] == '0'
+1 <= minJump <= maxJump < s.length
+
+*/
+#include"1871.JumpGameVII.h"
+int main() 
+ {
+ 	 Solution s;
+ 	 cout<<s.canReach("011010",2,3)<<endl;
+ 	 cout<<s.canReach("01101110",2,3)<<endl;
+ 	 return 0;
+ }
