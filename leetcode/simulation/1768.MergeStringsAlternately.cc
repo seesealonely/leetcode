35b1f178bdee1b4bf22a4d3617ce194b24d4a2af
seesea <seesealonely@gmail.com>
2022-01-25 20:53:39 +0800

    simulation

---
+++ leetcode/simulation/1768.MergeStringsAlternately.cc
@@ -0,0 +1,65 @@
+/*
+   You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+   Return the merged string.
+
+
+
+   Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1: a b c
+word2: p q r
+merged: a p b q c r
+
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1: a b
+word2: p q r s
+merged: a p b q r s
+
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1: a b c d
+word2: p q
+merged: a p b q c d
+
+
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+*/
+#include"head.h"
+class Solution {
+	public:
+		string mergeAlternately(string word1, string word2) {
+		const int len1=word1.size(),len2=word2.size();
+		string res;
+		int i=0,j=0;
+		while(i<len1&&j<len2)
+			res+=word1[i++],res+=word2[j++];
+		if(i==len1)
+			res+=word2.substr(j);
+		else
+			res+=word1.substr(i);
+		return res;
+		}
+};
+int main() 
+{
+	Solution s;
+	cout<<s.mergeAlternately("abc","pqr")<<endl;
+	cout<<s.mergeAlternately("ab","pqrs")<<endl;
+	cout<<s.mergeAlternately("abcd","pq")<<endl;
+	return 0;
+}
