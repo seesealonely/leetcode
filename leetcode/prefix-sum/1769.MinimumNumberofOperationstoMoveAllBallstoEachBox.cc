4658711658434ecf78cde7293d86da626162f86d
seesea <seesealonely@gmail.com>
2022-01-26 02:28:50 +0800

    presum + suffsum

---
+++ leetcode/prefix-sum/1769.MinimumNumberofOperationstoMoveAllBallstoEachBox.cc
@@ -0,0 +1,69 @@
+/*
+   You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+
+   In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+
+   Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+
+   Each answer[i] is calculated considering the initial state of the boxes.
+
+
+
+   Example 1:
+
+Input: boxes = "110"
+Output: [1,1,3]
+Explanation: The answer for each box is as follows:
+1) First box: you will have to move one ball from the second box to the first box in one operation.
+2) Second box: you will have to move one ball from the first box to the second box in one operation.
+3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+
+Example 2:
+
+Input: boxes = "001011"
+Output: [11,8,5,4,3,4]
+
+
+
+Constraints:
+
+n == boxes.length
+1 <= n <= 2000
+boxes[i] is either '0' or '1'.
+
+*/
+#include"head.h"
+class Solution {
+	public:
+		vector<int> minOperations(string boxes) {
+			const int n=boxes.size();
+			vector<int> presum(n,0),sufsum(n,0);
+			int cnt=0;
+			for(int i=0;i<n;i++)
+			{
+				presum[i]+=cnt+(i==0?0:presum[i-1]);
+				if(boxes[i]=='1')
+					cnt++;
+			}	
+			cnt=0;
+			for(int i=n-1;i>=0;i--)
+			{
+				sufsum[i]=cnt+(i==n-1?0:sufsum[i+1]);
+				if(boxes[i]=='1')
+					cnt++;
+			}
+
+			vector<int> res;
+			for(int i=0;i<n;i++)
+			res.push_back(presum[i]+sufsum[i]);
+
+			return res;
+		}
+};
+int main() 
+{
+	Solution s;
+	show(s.minOperations("110"));
+	show(s.minOperations("001011"));
+	return 0;
+}
