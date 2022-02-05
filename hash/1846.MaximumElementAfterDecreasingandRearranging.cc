b6053aace719a21188ae69a5d9ee4d77f276ff01
seesea <seesealonely@gmail.com>
2022-02-06 01:59:27 +0800

    hash+greedy

---
+++ hash/1846.MaximumElementAfterDecreasingandRearranging.cc
@@ -0,0 +1,82 @@
+/*
+   You are given an array of positive integers arr. Perform some operations (possibly none) on arr so that it satisfies these conditions:
+
+   The value of the first element in arr must be 1.
+   The absolute difference between any 2 adjacent elements must be less than or equal to 1. In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). abs(x) is the absolute value of x.
+
+   There are 2 types of operations that you can perform any number of times:
+
+   Decrease the value of any element of arr to a smaller positive integer.
+   Rearrange the elements of arr to be in any order.
+
+   Return the maximum possible value of an element in arr after performing the operations to satisfy the conditions.
+
+
+
+   Example 1:
+
+Input: arr = [2,2,1,2,1]
+Output: 2
+Explanation:
+We can satisfy the conditions by rearranging arr so it becomes [1,2,2,2,1].
+The largest element in arr is 2.
+
+Example 2:
+
+Input: arr = [100,1,1000]
+Output: 3
+Explanation:
+One possible way to satisfy the conditions is by doing the following:
+1. Rearrange arr so it becomes [1,100,1000].
+2. Decrease the value of the second element to 2.
+3. Decrease the value of the third element to 3.
+Now arr = [1,2,3], which satisfies the conditions.
+The largest element in arr is 3.
+
+Example 3:
+
+Input: arr = [1,2,3,4,5]
+Output: 5
+Explanation: The array already satisfies the conditions, and the largest element is 5.
+
+
+
+Constraints:
+
+1 <= arr.length <= 105
+1 <= arr[i] <= 109
+
+*/
+#include"head.h"
+class Solution {
+	public:
+		int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+			const int n=arr.size();
+			vector<int> count(n+1,0);
+			for(int i=0;i<n;i++)
+				count[min(n,arr[i])]++;
+			
+			int miss=0;
+			for(int i=1;i<=n;i++)
+			{
+				if(count[i]==0)
+					miss++;
+				else
+					miss-=min(count[i]-1,miss);
+			}
+
+			return n-miss;
+
+		}
+};
+int main() 
+{
+	Solution s;
+	vector<int> v={2,2,1,2,1};
+	cout<<s.maximumElementAfterDecrementingAndRearranging(v)<<endl;
+	v.clear();v={100,1,1000};
+	cout<<s.maximumElementAfterDecrementingAndRearranging(v)<<endl;
+	v.clear();v={1,2,3,4,5};
+	cout<<s.maximumElementAfterDecrementingAndRearranging(v)<<endl;
+	return 0;
+}
