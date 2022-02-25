27b2a720ed4d2e3a747a500f63ce9bda1e5a5f8d
seesea <seesealonely@gmail.com>
2022-02-26 03:54:31 +0800

    presum + binary search

---
+++ prefix-sum/1712.WaystoSplitArrayIntoThreeSubarrays.cc
@@ -0,0 +1,72 @@
+/*
+   A split of an integer array is good if:
+
+   The array is split into three non-empty contiguous subarrays - named left, mid, right respectively from left to right.
+   The sum of the elements in left is less than or equal to the sum of the elements in mid, and the sum of the elements in mid is less than or equal to the sum of the elements in right.
+
+   Given nums, an array of non-negative integers, return the number of good ways to split nums. As the number may be too large, return it modulo 109 + 7.
+
+
+
+   Example 1:
+
+Input: nums = [1,1,1]
+Output: 1
+Explanation: The only good way to split nums is [1] [1] [1].
+
+Example 2:
+
+Input: nums = [1,2,2,2,5,0]
+Output: 3
+Explanation: There are three good ways of splitting nums:
+[1] [2] [2,2,5,0]
+[1] [2,2] [2,5,0]
+[1,2] [2,2] [5,0]
+
+Example 3:
+
+Input: nums = [3,2,1]
+Output: 0
+Explanation: There is no good way to split nums.
+
+
+
+Constraints:
+
+3 <= nums.length <= 105
+0 <= nums[i] <= 104
+*/
+#include"head.h"
+class Solution {
+    public:
+        int waysToSplit(vector<int>& nums) {
+        const int n=nums.size();
+        vector<int> presum(n+1,0);
+
+        for(int i=0;i<n;i++)
+            presum[i+1]=presum[i]+nums[i];
+
+        int res=0;
+        for(int i=1;i<n-1;i++)
+        {
+                int second=lower_bound(presum.begin()+i,presum.end(),2*presum[i])-presum.begin();
+                int third=upper_bound(presum.begin()+i,presum.end(),presum[i]+(presum.back()-presum[i])/2)-presum.begin();
+            
+                if(second>third)
+                    continue;
+                res+=third-second;
+        }
+        return res;
+        }
+};
+int main() 
+{
+    Solution s;
+    vector<int> v={1,1,1};
+    cout<<s.waysToSplit(v)<<endl;
+    v.clear();v={1,2,2,2,5,0};
+    cout<<s.waysToSplit(v)<<endl;
+    v.clear();v={3,2,1};
+    cout<<s.waysToSplit(v)<<endl;
+    return 0;
+}
