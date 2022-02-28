95f6d62c91ff421ed3217c5a5b496c38f81a9bb5
seesea <seesealonely@gmail.com>
2022-03-01 05:22:30 +0800

    presum

---
+++ prefix-sum/1749.MaximumAbsoluteSumofAnySubarray.cc
@@ -0,0 +1,56 @@
+/*
+   You are given an integer array nums. The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+
+   Return the maximum absolute sum of any (possibly empty) subarray of nums.
+
+   Note that abs(x) is defined as follows:
+
+   If x is a negative integer, then abs(x) = -x.
+   If x is a non-negative integer, then abs(x) = x.
+
+
+
+   Example 1:
+
+Input: nums = [1,-3,2,3,-4]
+Output: 5
+Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+
+Example 2:
+
+Input: nums = [2,-5,1,-4,3,-2]
+Output: 8
+Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
+
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+
+*/
+#include"head.h"
+class Solution {
+    public:
+        int maxAbsoluteSum(vector<int>& nums) {
+        int sum=0,mx=0,mn=0,res=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            res=max(res,max(abs(sum-mx),abs(sum-mn)));
+            mx=max(mx,sum);
+            mn=min(mn,sum);
+        }
+        return res;
+        }
+};
+int main() 
+{
+    Solution s;
+    vector<int> v={1,-3,2,3,-4};
+    cout<<s.maxAbsoluteSum(v)<<endl;
+    v.clear();v={2,-5,1,-4,3,-2};
+    cout<<s.maxAbsoluteSum(v)<<endl;
+    return 0;
+}
