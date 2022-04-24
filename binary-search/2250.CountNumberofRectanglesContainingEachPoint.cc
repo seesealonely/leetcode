7734bfd3390bf7561d2b8401fe942b339585e8bb
seesealonely <seesealonely@gmail.com>
2022-04-25 03:37:51 +0800

    binary search + bucket + sort

---
+++ binary-search/2250.CountNumberofRectanglesContainingEachPoint.cc
@@ -0,0 +1,82 @@
+/*
+   You are given a 2D integer array rectangles where rectangles[i] = [li, hi] indicates that ith rectangle has a length of li and a height of hi. You are also given a 2D integer array points where points[j] = [xj, yj] is a point with coordinates (xj, yj).
+
+   The ith rectangle has its bottom-left corner point at the coordinates (0, 0) and its top-right corner point at (li, hi).
+
+   Return an integer array count of length points.length where count[j] is the number of rectangles that contain the jth point.
+
+   The ith rectangle contains the jth point if 0 <= xj <= li and 0 <= yj <= hi. Note that points that lie on the edges of a rectangle are also considered to be contained by that rectangle.
+
+
+
+   Example 1:
+
+Input: rectangles = [[1,2],[2,3],[2,5]], points = [[2,1],[1,4]]
+Output: [2,1]
+Explanation: 
+The first rectangle contains no points.
+The second rectangle contains only the point (2, 1).
+The third rectangle contains the points (2, 1) and (1, 4).
+The number of rectangles that contain the point (2, 1) is 2.
+The number of rectangles that contain the point (1, 4) is 1.
+Therefore, we return [2, 1].
+
+Example 2:
+
+Input: rectangles = [[1,1],[2,2],[3,3]], points = [[1,3],[1,1]]
+Output: [1,3]
+Explanation:
+The first rectangle contains only the point (1, 1).
+The second rectangle contains only the point (1, 1).
+The third rectangle contains the points (1, 3) and (1, 1).
+The number of rectangles that contain the point (1, 3) is 1.
+The number of rectangles that contain the point (1, 1) is 3.
+Therefore, we return [1, 3].
+
+
+
+Constraints:
+
+1 <= rectangles.length, points.length <= 5 * 104
+rectangles[i].length == points[j].length == 2
+1 <= li, xj <= 109
+1 <= hi, yj <= 100
+All the rectangles are unique.
+All the points are unique.
+*/
+
+
+#include"head.h"
+class Solution {
+    public:
+        vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
+            vector<vector<int>> hash(101);
+            for(int i=0;i<rectangles.size();i++)
+                hash[rectangles[i][1]].push_back(rectangles[i][0]);
+
+            for(int i=0;i<101;i++)
+                sort(hash[i].begin(),hash[i].end());
+
+            vector<int> res;
+            for(int i=0;i<points.size();i++)
+            {
+                int x=points[i][0],y=points[i][1],sum=0;
+                for(int j=y;j<=100;j++)
+                    sum+=hash[j].end()-lower_bound(hash[j].begin(),hash[j].end(),x); 
+                res.push_back(sum);
+            }
+            return res;
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<vector<int>> r={{1,2},{2,3},{2,5}},p={{2,1},{1,4}};
+    show(s.countRectangles(r,p));
+    r.clear();r={{1,1},{2,2},{3,3}};
+    p.clear();p={{1,3},{1,1}};
+    show(s.countRectangles(r,p));
+    return 0;
+}
