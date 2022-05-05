779e88aedd94f89eb2d229a6f7887192902ace8f
seesealonely <seesealonely@gmail.com>
2022-05-05 20:23:33 +0800

    topological sort

---
+++ topological-sort/2115.FindAllPossibleRecipesfromGivenSupplies.cc
@@ -0,0 +1,102 @@
+/*
+   You have information about n different recipes. You are given a string array recipes and a 2D string array ingredients. The ith recipe has the name recipes[i], and you can create it if you have all the needed ingredients from ingredients[i]. Ingredients to a recipe may need to be created from other recipes, i.e., ingredients[i] may contain a string that is in recipes.
+
+   You are also given a string array supplies containing all the ingredients that you initially have, and you have an infinite supply of all of them.
+
+   Return a list of all the recipes that you can create. You may return the answer in any order.
+
+   Note that two recipes may contain each other in their ingredients.
+
+
+
+   Example 1:
+
+Input: recipes = ["bread"], ingredients = [["yeast","flour"]], supplies = ["yeast","flour","corn"]
+Output: ["bread"]
+Explanation:
+We can create "bread" since we have the ingredients "yeast" and "flour".
+
+Example 2:
+
+Input: recipes = ["bread","sandwich"], ingredients = [["yeast","flour"],["bread","meat"]], supplies = ["yeast","flour","meat"]
+Output: ["bread","sandwich"]
+Explanation:
+We can create "bread" since we have the ingredients "yeast" and "flour".
+We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
+
+Example 3:
+
+Input: recipes = ["bread","sandwich","burger"], ingredients = [["yeast","flour"],["bread","meat"],["sandwich","meat","bread"]], supplies = ["yeast","flour","meat"]
+Output: ["bread","sandwich","burger"]
+Explanation:
+We can create "bread" since we have the ingredients "yeast" and "flour".
+We can create "sandwich" since we have the ingredient "meat" and can create the ingredient "bread".
+We can create "burger" since we have the ingredient "meat" and can create the ingredients "bread" and "sandwich".
+
+
+
+Constraints:
+
+n == recipes.length == ingredients.length
+1 <= n <= 100
+1 <= ingredients[i].length, supplies.length <= 100
+1 <= recipes[i].length, ingredients[i][j].length, supplies[k].length <= 10
+recipes[i], ingredients[i][j], and supplies[k] consist only of lowercase English letters.
+All the values of recipes and supplies combined are unique.
+Each ingredients[i] does not contain any duplicate values.
+
+*/
+
+#define c11
+#include"head.h"
+class Solution {
+    public:
+        vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+            unordered_map<string,unordered_set<string>> g;
+            unordered_map<string,int> indegree;
+
+            for(int i=0;i<ingredients.size();i++)
+            {
+                for(int j=0;j<ingredients[i].size();j++)
+                {
+                    g[ingredients[i][j]].insert(recipes[i]);
+                    indegree[recipes[i]]++;
+                }
+            }
+
+            vector<string> res;
+            queue<string> q;
+            for(int i=0;i<supplies.size();i++)
+                q.push(supplies[i]);
+
+            while(!q.empty())
+            {
+                string cur=q.front();q.pop();
+                for(unordered_set<string>::iterator it=g[cur].begin();it!=g[cur].end();it++)
+                    if(--indegree[*it]==0)
+                    {
+                        q.push(*it);
+                        res.push_back(*it);
+                    }
+            }
+            return res;
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<string> r={"bread"},su={"yeast","flour","corn"};
+    vector<vector<string>> i={{"yeast","flour"}};
+    show(s.findAllRecipes(r,i,su));
+    r.clear();r={"bread","sandwich"};
+    su.clear();su={"yeast","flour","meat"};
+    i.clear();i={{"yeast","flour"},{"bread","meat"}};
+    show(s.findAllRecipes(r,i,su));
+    r.clear();r={"bread","sandwich","burger"};
+    su.clear();su={"yeast","flour","meat"};
+    i.clear();i={{"yeast","flour"},{"bread","meat"},{"sandwich","meat","bread"}};
+    show(s.findAllRecipes(r,i,su));
+    return 0;
+}
