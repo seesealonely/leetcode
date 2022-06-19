2bb3855b65bbd167adfa6f8e57bb9e1c12ef9069
seesealonely <seesealonely@gmail.com>
2022-06-20 03:59:08 +0800

    math + gcd

---
+++ math/592.FractionAdditionandSubtraction.cc
@@ -0,0 +1,71 @@
+/*
+   Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
+
+   The final result should be an irreducible fraction. If your final result is an integer, change it to the format of a fraction that has a denominator 1. So in this case, 2 should be converted to 2/1.
+
+
+
+   Example 1:
+
+Input: expression = "-1/2+1/2"
+Output: "0/1"
+
+Example 2:
+
+Input: expression = "-1/2+1/2+1/3"
+Output: "1/3"
+
+Example 3:
+
+Input: expression = "1/3-1/2"
+Output: "-1/6"
+
+
+
+Constraints:
+
+The input string only contains '0' to '9', '/', '+' and '-'. So does the output.
+Each fraction (input and output) has the format ±numerator/denominator. If the first input fraction or the output is positive, then '+' will be omitted.
+The input only contains valid irreducible fractions, where the numerator and denominator of each fraction will always be in the range [1, 10]. If the denominator is 1, it means this fraction is actually an integer in a fraction format defined above.
+The number of given fractions will be in the range [1, 10].
+The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
+
+*/
+
+
+#include"head.h"
+class Solution {
+    public:
+        string fractionAddition(string expression) {
+            istringstream is(expression);
+            char ch;
+            int a=0,b=1,c,d;
+            while(is>>c>>ch>>d)
+            {
+                a=a*d+b*c;
+                b=b*d;
+                const int d=abs(gcd(a,b));
+                a/=d;
+                b/=d;
+            }
+            return to_string(a)+"/"+to_string(b);
+        }
+        int gcd(int a,int b)
+        {
+            if(!(a%b))
+                return b;
+            else
+                return gcd(b,a%b);
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    cout<<s.fractionAddition("-1/2+1/2")<<endl;
+    cout<<s.fractionAddition("-1/2+1/2+1/3")<<endl;
+    cout<<s.fractionAddition("1/3-1/2")<<endl;
+    cout<<s.fractionAddition("5/3+1/3")<<endl;
+    return 0;
+}
