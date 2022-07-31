39c9f461758140844b43030b033420532ed6ecf5
seesealonely <seesealonely@gmail.com>
2022-08-01 03:38:50 +0800

    hash

---
+++ hash/2357.MakeArrayZerobySubtractingEqualAmounts.cc
@@ -0,0 +1,59 @@
+/*
+   You are given a non-negative integer array nums. In one operation, you must:
+
+   Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+   Subtract x from every positive element in nums.
+
+   Return the minimum number of operations to make every element in nums equal to 0.
+
+
+
+   Example 1:
+
+Input: nums = [1,5,0,3,5]
+Output: 3
+Explanation:
+In the first operation, choose x = 1. Now, nums = [0,4,0,2,4].
+In the second operation, choose x = 2. Now, nums = [0,2,0,0,2].
+In the third operation, choose x = 2. Now, nums = [0,0,0,0,0].
+
+Example 2:
+
+Input: nums = [0]
+Output: 0
+Explanation: Each element in nums is already 0 so no operations are needed.
+
+
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+
+*/
+
+#define c11
+#include"head.h"
+class Solution {
+    public:
+        int minimumOperations(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i])
+                s.insert(nums[i]);
+
+        return s.size();
+
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<int> v={1,5,0,3,5};
+    cout<<s.minimumOperations(v)<<endl;
+    v.clear();v={0};
+    cout<<s.minimumOperations(v)<<endl;
+    return 0;
+}
