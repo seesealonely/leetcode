9ad280a4e40419a250e4e6ddc331de37b11fe244
seesealonely <seesealonely@gmail.com>
2022-10-27 07:32:14 +0800

    l.sh

--- simulation/l.sh
+++ simulation/l.sh
@@ -12,11 +12,11 @@ cclass=class
 fenjie='/*'
 echo "$fenjie" >>$quesionname.cc
 while read line;do
-	if [ "${line%% *}" = "$cclass" ] 
-	then
-	break
-	fi
-	echo $line >>$quesionname.cc
+    if [ "${line%% *}" = "$cclass" ] 
+    then
+        break
+    fi
+    echo $line >>$quesionname.cc
 done
 unset fenjie
 fenjie='*/' 
@@ -24,27 +24,33 @@ echo "$fenjie">>$quesionname.cc
 
 echo -e "\n">>$quesionname.cc
 cinclude="#include\"head.h\""
-	echo $cinclude >>$quesionname.cc
-	echo $line >>$quesionname.cc
+echo $cinclude >>$quesionname.cc
+echo $line >>$quesionname.cc
+
 end='};'
+i=1
 while read line;do
-	echo $line >>$quesionname.cc
+    i=$[$i+1]
+    echo $line >>$quesionname.cc
     if [ "${line:-2}" == $end ]
     then 
         break
     fi
+    if [ $i == "3" ]
+    then
+        classname=${line%%(*}
+        classname=${classname##* }
+    fi
 done
 
-classname=$quesionname
-maintext="int main() \n
+main="int main() \n
 {\n
-\t	Solution s;\n
-\t	s.${classname#*.}();\n
-\t	return 0;\n
+    \t	Solution s;\n
+    \t	s.$classname();\n
+    \t	return 0;\n
 }"
 echo -e "\n">>$quesionname.cc
-echo -e $maintext >> $quesionname.cc
+echo -e $main>> $quesionname.cc
 
-$quesionname
-echo ${quesionname#*.} >>makefile
+echo ${quesionname} >>makefile
 echo "end all input operation"
