a1532c2432951606ff699d1421a2b5c3711ded9e
seesealonely <seesealonely@gmail.com>
2022-10-27 20:17:33 +0800

    thinking

---
+++ thinking/2017.GridGame.cc
@@ -0,0 +1,19 @@
+/*
+*/
+
+
+#include"head.h"
+class Solution {
+public:
+long long gridGame(vector<vector<int>>& grid) {
+
+}
+};
+
+
+int main() 
+ {
+ 	 Solution s;
+ 	 s.gridGame();
+ 	 return 0;
+ }
