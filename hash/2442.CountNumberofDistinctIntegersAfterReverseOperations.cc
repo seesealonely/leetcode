eaa39865fe5e496450f010fa28c8876216ede17a
seesealonely <seesealonely@gmail.com>
2022-11-01 06:40:22 +0800

    unordered_set

---
+++ hash/2442.CountNumberofDistinctIntegersAfterReverseOperations.cc
@@ -0,0 +1,64 @@
+/*
+   You are given an array nums consisting of positive integers.
+
+   You have to take each integer in the array, reverse its digits, and add it to the end of the array. You should apply this operation to the original integers in nums.
+
+   Return the number of distinct integers in the final array.
+
+
+
+   Example 1:
+
+Input: nums = [1,13,10,12,31]
+Output: 6
+Explanation: After including the reverse of each number, the resulting array is [1,13,10,12,31,1,31,1,21,13].
+The reversed integers that were added to the end of the array are underlined. Note that for the integer 10, after reversing it, it becomes 01 which is just 1.
+The number of distinct integers in this array is 6 (The numbers 1, 10, 12, 13, 21, and 31).
+
+Example 2:
+
+Input: nums = [2,2,2]
+Output: 1
+Explanation: After including the reverse of each number, the resulting array is [2,2,2,2,2,2].
+The number of distinct integers in this array is 1 (The number 2).
+
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+
+*/
+
+#define c11
+#include"head.h"
+class Solution {
+    public:
+        int countDistinctIntegers(vector<int>& nums) {
+        unordered_set<int> exist(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++)
+        {
+            int cur=nums[i],rev=0;
+            while(cur)
+            {
+                rev=rev*10+cur%10;
+                cur/=10;
+            }
+            exist.insert(rev);
+        }
+        return exist.size();
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<int> v={1,13,10,12,31};
+    cout<<s.countDistinctIntegers(v)<<endl;
+    v.clear();v={2,2,2};
+    cout<<s.countDistinctIntegers(v)<<endl;
+    return 0;
+}
