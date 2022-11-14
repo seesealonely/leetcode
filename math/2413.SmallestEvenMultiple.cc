a669144150b922bacbe5d1ee327dbe77737b57f6
seesealonely <seesealonely@gmail.com>
2022-11-14 15:21:06 +0800

    math

---
+++ math/2413.SmallestEvenMultiple.cc
@@ -0,0 +1,42 @@
+/*
+
+   Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+
+
+
+   Example 1:
+
+Input: n = 5
+Output: 10
+Explanation: The smallest multiple of both 5 and 2 is 10.
+
+Example 2:
+
+Input: n = 6
+Output: 6
+Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+
+
+
+Constraints:
+
+1 <= n <= 150
+*/
+
+
+#include"head.h"
+class Solution {
+    public:
+        int smallestEvenMultiple(int n) {
+        return n%2==0?n:2*n;
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    cout<<s.smallestEvenMultiple(5)<<endl;
+    cout<<s.smallestEvenMultiple(6)<<endl;
+    return 0;
+}
