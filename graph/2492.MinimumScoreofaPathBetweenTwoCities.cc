3101ac640ee1979f7b1aeef35daa16118d99bf03
seesealonely <seesealonely@gmail.com>
2022-12-07 08:43:51 +0800

    graph + bfs

---
+++ graph/2492.MinimumScoreofaPathBetweenTwoCities.cc
@@ -0,0 +1,90 @@
+/*
+   You are given a positive integer n representing n cities numbered from 1 to n. You are also given a 2D array roads where roads[i] = [ai, bi, distancei] indicates that there is a bidirectional road between cities ai and bi with a distance equal to distancei. The cities graph is not necessarily connected.
+
+   The score of a path between two cities is defined as the minimum distance of a road in this path.
+
+   Return the minimum possible score of a path between cities 1 and n.
+
+Note:
+
+A path is a sequence of roads between two cities.
+It is allowed for a path to contain the same road multiple times, and you can visit cities 1 and n multiple times along the path.
+The test cases are generated such that there is at least one path between 1 and n.
+
+ 
+
+Example 1:
+
+Input: n = 4, roads = [[1,2,9],[2,3,6],[2,4,5],[1,4,7]]
+Output: 5
+Explanation: The path from city 1 to 4 with the minimum score is: 1 -> 2 -> 4. The score of this path is min(9,5) = 5.
+It can be shown that no other path has less score.
+
+Example 2:
+
+Input: n = 4, roads = [[1,2,2],[1,3,4],[3,4,7]]
+Output: 2
+Explanation: The path from city 1 to 4 with the minimum score is: 1 -> 2 -> 1 -> 3 -> 4. The score of this path is min(2,2,4,7) = 2.
+
+ 
+
+Constraints:
+
+2 <= n <= 105
+1 <= roads.length <= 105
+roads[i].length == 3
+1 <= ai, bi <= n
+ai != bi
+1 <= distancei <= 104
+There are no repeated edges.
+There is at least one path between 1 and n.
+
+*/
+
+
+#include"head.h"
+class Solution {
+    public:
+        int minScore(int n, vector<vector<int>>& roads) {
+        const int m=roads.size();
+        vector<vector<pair<int,int>>> g(n);
+
+        for(int i=0;i<m;i++)
+        {
+            int x=roads[i][0]-1,y=roads[i][1]-1,l=roads[i][2];
+            g[x].push_back({y,l});
+            g[y].push_back({x,l});
+        }
+
+        queue<int> q;q.push(0);
+        vector<bool> vis(n,false); vis[0]=true;
+        int res=INT_MAX;
+
+        while(!q.empty())
+        {
+            int u=q.front();q.pop();
+            for(int i=0;i<g[u].size();i++)
+            {
+                pair<int,int> v=g[u][i];
+                res=min(res,v.second);
+                if(!vis[v.first])
+                {
+                    vis[v.first]=true;
+                    q.push(v.first);
+                }
+            }
+        }
+        return res;
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<vector<int>> v={{1,2,9},{2,3,6},{2,4,5},{1,4,7}};
+    cout<<s.minScore(4,v)<<endl;
+    v.clear();v={{1,2,2},{1,3,4},{3,4,7}};
+    cout<<s.minScore(4,v)<<endl;
+    return 0;
+}
