bd2e13b467a85303fdbc10fa34511323c4ba25d3
seesealonely <seesealonely@gmail.com>
2022-12-29 21:53:28 +0800

    graph - dijkstra

---
+++ graph/2065.MaximumPathQualityofaGraph.cc
@@ -0,0 +1,147 @@
+/*
+   There is an undirected graph with n nodes numbered from 0 to n - 1 (inclusive). You are given a 0-indexed integer array values where values[i] is the value of the ith node. You are also given a 0-indexed 2D integer array edges, where each edges[j] = [uj, vj, timej] indicates that there is an undirected edge between the nodes uj and vj, and it takes timej seconds to travel between the two nodes. Finally, you are given an integer maxTime.
+
+   A valid path in the graph is any path that starts at node 0, ends at node 0, and takes at most maxTime seconds to complete. You may visit the same node multiple times. The quality of a valid path is the sum of the values of the unique nodes visited in the path (each node's value is added at most once to the sum).
+
+   Return the maximum quality of a valid path.
+
+Note: There are at most four edges connected to each node.
+
+ 
+
+Example 1:
+
+Input: values = [0,32,10,43], edges = [[0,1,10],[1,2,15],[0,3,10]], maxTime = 49
+Output: 75
+Explanation:
+One possible path is 0 -> 1 -> 0 -> 3 -> 0. The total time taken is 10 + 10 + 10 + 10 = 40 <= 49.
+The nodes visited are 0, 1, and 3, giving a maximal path quality of 0 + 32 + 43 = 75.
+
+Example 2:
+
+Input: values = [5,10,15,20], edges = [[0,1,10],[1,2,10],[0,3,10]], maxTime = 30
+Output: 25
+Explanation:
+One possible path is 0 -> 3 -> 0. The total time taken is 10 + 10 = 20 <= 30.
+The nodes visited are 0 and 3, giving a maximal path quality of 5 + 20 = 25.
+
+Example 3:
+
+Input: values = [1,2,3,4], edges = [[0,1,10],[1,2,11],[2,3,12],[1,3,13]], maxTime = 50
+Output: 7
+Explanation:
+One possible path is 0 -> 1 -> 3 -> 1 -> 0. The total time taken is 10 + 13 + 13 + 10 = 46 <= 50.
+The nodes visited are 0, 1, and 3, giving a maximal path quality of 1 + 2 + 4 = 7.
+
+ 
+
+Constraints:
+
+n == values.length
+1 <= n <= 1000
+0 <= values[i] <= 108
+0 <= edges.length <= 2000
+edges[j].length == 3
+0 <= uj < vj <= n - 1
+10 <= timej, maxTime <= 100
+All the pairs [uj, vj] are unique.
+There are at most four edges connected to each node.
+The graph may not be connected.
+
+*/
+
+
+#include"head.h"
+class Solution {
+    public:
+        int n,res;
+        vector<vector<pair<int,int>>> g;
+        vector<bool> vis;
+        vector<int> dis;
+
+        void dijkstra(int maxTime)
+        {
+            dis.resize(n,INT_MAX);
+
+            priority_queue<pair<int,int>> q;
+            dis[0]=0;
+            q.push({0,0});
+
+            while(!q.empty())
+            {
+                int u=q.top().second;q.pop();
+
+                for(int i=0;i<g[u].size();i++)
+                {
+                    int v=g[u][i].first,t=g[u][i].second;
+
+                    if(dis[u]+t<=maxTime&&dis[v]>dis[u]+t)
+                    {
+                        dis[v]=dis[u]+t;
+                        q.push({-dis[v],v});
+                    }
+                }
+            }
+        }
+
+        void solve(int u,int sum,int time,const vector<int> &values)
+        {
+            if(u==0&&res<sum)
+                res=sum;
+
+            if(time<dis[u])
+                return ;
+
+            for(int i=0;i<g[u].size();i++)
+            {
+                int v=g[u][i].first,t=g[u][i].second;
+
+                if(time<t)
+                    continue;
+
+                if(vis[v])
+                    solve(v,sum,time-t,values);
+                else
+                {
+                    vis[v]=true;
+                    solve(v,sum+values[v],time-t,values);
+                    vis[v]=false;
+                }
+            }
+        }
+        int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime) {
+        n=values.size();
+        g.resize(n);
+        for(int i=0;i<edges.size();i++)
+        {
+            int x=edges[i][0],y=edges[i][1],v=edges[i][2];
+            g[x].push_back({y,v});
+            g[y].push_back({x,v});
+        }
+
+        dijkstra(maxTime);
+
+        res=0;
+        vis.resize(n,false);
+        vis[0]=true;
+        solve(0,values[0],maxTime,values);
+
+        return res;
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<int> v={0,32,10,43};
+    vector<vector<int>> e={{0,1,10},{1,2,15},{0,3,10}};
+    cout<<s.maximalPathQuality(v,e,49)<<endl;
+    v.clear();v={5,10,15,20};
+    e.clear();e={{0,1,10},{1,2,10},{0,3,10}};
+    cout<<s.maximalPathQuality(v,e,30)<<endl;
+    v.clear();v={1,2,3,4};
+    e.clear();e={{0,1,10},{1,2,11},{2,3,12},{1,3,13}};
+    cout<<s.maximalPathQuality(v,e,50)<<endl;
+    return 0;
+}
