1ef1677d89c9ba207f74f29fa07cfd2cab257ad7
seesealonely <seesealonely@gmail.com>
2023-01-11 04:05:18 +0800

    priority queue

---
+++ priority-queue/2530.MaximalScoreAfterApplyingKOperations.cc
@@ -0,0 +1,71 @@
+/*
+   You are given a 0-indexed integer array nums and an integer k. You have a starting score of 0.
+
+   In one operation:
+
+   choose an index i such that 0 <= i < nums.length,
+   increase your score by nums[i], and
+   replace nums[i] with ceil(nums[i] / 3).
+
+   Return the maximum possible score you can attain after applying exactly k operations.
+
+   The ceiling function ceil(val) is the least integer greater than or equal to val.
+
+    
+
+   Example 1:
+
+Input: nums = [10,10,10,10,10], k = 5
+Output: 50
+Explanation: Apply the operation to each array element exactly once. The final score is 10 + 10 + 10 + 10 + 10 = 50.
+
+Example 2:
+
+Input: nums = [1,10,3,3,3], k = 3
+Output: 17
+Explanation: You can do the following operations:
+Operation 1: Select i = 1, so nums becomes [1,4,3,3,3]. Your score increases by 10.
+Operation 2: Select i = 1, so nums becomes [1,2,3,3,3]. Your score increases by 4.
+Operation 3: Select i = 2, so nums becomes [1,1,1,3,3]. Your score increases by 3.
+The final score is 10 + 4 + 3 = 17.
+
+ 
+
+Constraints:
+
+1 <= nums.length, k <= 105
+1 <= nums[i] <= 109
+
+*/
+
+
+#include"head.h"
+class Solution {
+    public:
+        long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> heap(nums.begin(),nums.end());
+        
+        long long res=0;
+        while(k--)
+        {
+            int mx=heap.top();
+            heap.pop();
+
+            res+=mx;
+            heap.push(ceil((double)mx/3));
+        }
+
+        return res;
+        }
+};
+
+
+int main() 
+{
+    Solution s;
+    vector<int> v={10,10,10,10,10};
+    cout<<s.maxKelements(v,5)<<endl;
+    v.clear();v={1,10,3,3,3};
+    cout<<s.maxKelements(v,3)<<endl;
+    return 0;
+}
