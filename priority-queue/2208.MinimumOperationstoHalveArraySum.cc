69f4e5732021d0393449d0b6ef0994fdc491a6aa
seesealonely <seesealonely@gmail.com>
2023-02-12 18:51:25 +0800

    priority queue

--- priority-queue/2208.MinimumOperationstoHalveArraySum.cc
+++ priority-queue/2208.MinimumOperationstoHalveArraySum.cc
@@ -29,7 +29,7 @@ Pick the number 20 and reduce it to 10.
 Pick the number 10 and reduce it to 5.
 Pick the number 3 and reduce it to 1.5.
 The final array is [1.5, 8, 5] with a total sum of 1.5 + 8 + 5 = 14.5.
-The sum of nums has been reduced by 31 - 14.5 = 16.5, which is at least half of the initial sum, 16.5 >= 31/2 = 16.5.
+The sum of nums has been reduced by 31 - 14.5 = 16.5, which is at least half of the initial sum, 16.5 >= 31/2 = 15.5.
 Overall, 3 operations were used so we return 3.
 It can be shown that we cannot reduce the sum by at least half in less than 3 operations.
 
@@ -47,23 +47,22 @@ Constraints:
 class Solution {
     public:
         int halveArray(vector<int>& nums) {
-            priority_queue<double> pq;
-            int res=0;
-            double s=0.0,t=0.0;
-            for(int i=0;i<nums.size();i++)
-            {
-                s+=(double)nums[i];
-                pq.push((double)nums[i]);
-            }
-            s/=2;
-            while(t<s)
-            {
-                double cur=pq.top();pq.pop();
-                t+=(cur/=2);
-                pq.push(cur);
-                res++;
-            }
-            return res;
+        long long sum=accumulate(nums.begin(),nums.end(),0LL);
+
+        priority_queue<double> q(nums.begin(),nums.end());
+
+        double cur=sum;
+        int res=0;
+        while(2*cur>sum)
+        {
+            double c=q.top();
+            q.pop();
+
+            cur-=c/2;
+            q.push(c/2);
+            res++;
+        }
+        return res;
         }
 };
 
@@ -75,5 +74,5 @@ int main()
     cout<<s.halveArray(v)<<endl;
     v.clear();v={3,8,20};
     cout<<s.halveArray(v)<<endl;
-        return 0;
-    }
+    return 0;
+}
