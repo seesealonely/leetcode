ccc8496a58b50f83721744f7a3b884f12b3ae0fe
seesealonely <seesealonely@gmail.com>
2023-03-15 12:36:26 +0800

    greedy

---
+++ greedy/2587.RearrangeArraytoMaximizePrefixScore.cc
@@ -0,0 +1,61 @@
+/*
+   You are given a 0-indexed integer array nums. You can rearrange the elements of nums to any order (including the given order).
+
+   Let prefix be the array containing the prefix sums of nums after rearranging it. In other words, prefix[i] is the sum of the elements from 0 to i in nums after rearranging it. The score of nums is the number of positive integers in the array prefix.
+
+   Return the maximum score you can achieve.
+
+
+
+   Example 1:
+
+Input: nums = [2,-1,0,1,-3,3,-3]
+Output: 6
+Explanation: We can rearrange the array into nums = [2,3,1,-1,-3,0,-3].
+prefix = [2,5,6,5,2,2,-1], so the score is 6.
+It can be shown that 6 is the maximum score we can obtain.
+
+Example 2:
+
+Input: nums = [-2,-3,0]
+Output: 0
+Explanation: Any rearrangement of the array will result in a score of 0.
+
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-106 <= nums[i] <= 106
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		int maxScore(vector<int>& nums) {
+		sort(nums.begin(),nums.end(),greater<int>());
+
+		long long n=nums.size(),s=0;
+		for(int i=0;i<n;i++)
+		{
+			s+=nums[i];
+			if(s<=0)
+				return i;
+		}
+
+		return n;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<int> v={2,-1,0,1,-3,3,-3};
+	cout<<s.maxScore(v)<<endl;
+	v.clear();v={-2,-3,0};
+	cout<<s.maxScore(v)<<endl;
+	return 0;
+}
