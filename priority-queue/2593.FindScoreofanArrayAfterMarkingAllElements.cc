190fc351871691902391dc8d87a937f0f0e48851
seesealonely <seesealonely@gmail.com>
2023-03-19 15:16:12 +0800

    priority queue

---
+++ priority-queue/2593.FindScoreofanArrayAfterMarkingAllElements.cc
@@ -0,0 +1,90 @@
+/*
+   You are given an array nums consisting of positive integers.
+
+   Starting with score = 0, apply the following algorithm:
+
+   Choose the smallest integer of the array that is not marked. If there is a tie, choose the one with the smallest index.
+   Add the value of the chosen integer to score.
+   Mark the chosen element and its two adjacent elements if they exist.
+   Repeat until all the array elements are marked.
+
+   Return the score you get after applying the above algorithm.
+
+
+
+   Example 1:
+
+Input: nums = [2,1,3,4,5,2]
+Output: 7
+Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,1,3,4,5,2].
+- 2 is the smallest unmarked element, so we mark it and its left adjacent element: [2,1,3,4,5,2].
+- 4 is the only remaining unmarked element, so we mark it: [2,1,3,4,5,2].
+Our score is 1 + 2 + 4 = 7.
+
+Example 2:
+
+Input: nums = [2,3,5,1,3,2]
+Output: 5
+Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,3,5,1,3,2].
+- 2 is the smallest unmarked element, since there are two of them, we choose the left-most one, so we mark the one at index 0 and its right adjacent element: [2,3,5,1,3,2].
+- 2 is the only remaining unmarked element, so we mark it: [2,3,5,1,3,2].
+Our score is 1 + 2 + 2 = 5.
+
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		long long findScore(vector<int>& nums) {
+
+		typedef long long ll;
+		typedef pair<ll,ll> pll;
+		const int n=nums.size();
+		priority_queue<pll,vector<pll>,greater<pll>> q;
+
+		for(int i=0;i<n;i++)
+		q.push({nums[i],i});
+
+		vector<bool> vis(n,false);
+		ll res=0;
+		while(!q.empty())
+		{
+			pll p=q.top();
+			q.pop();
+
+			if(vis[p.second])
+				continue;
+
+			res+=p.first;
+
+			if(p.second>0)
+				vis[p.second-1]=true;
+
+			if(p.second<n-1)
+				vis[p.second+1]=true;
+		}
+
+		return res;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<int> v={2,1,3,4,5,2};
+	cout<<s.findScore(v)<<endl;
+	v.clear();v={2,3,5,1,3,2};
+	cout<<s.findScore(v)<<endl;
+	return 0;
+}
