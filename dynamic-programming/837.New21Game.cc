3c7318dbbf3811467c8e7b5b7cce1ee95596a631
seesealonely <seesealonely@gmail.com>
2023-04-14 06:48:28 +0800

    dp

--- dynamic-programming/837.New21Game.cc
+++ dynamic-programming/837.New21Game.cc
@@ -1,17 +1,17 @@
 /*
-Alice plays the following game, loosely based on the card game "21".
+   Alice plays the following game, loosely based on the card game "21".
 
-Alice starts with 0 points and draws numbers while she has less than k points. During each draw, she gains an integer number of points randomly from the range [1, maxPts], where maxPts is an integer. Each draw is independent and the outcomes have equal probabilities.
+   Alice starts with 0 points and draws numbers while she has less than k points. During each draw, she gains an integer number of points randomly from the range [1, maxPts], where maxPts is an integer. Each draw is independent and the outcomes have equal probabilities.
 
-Alice stops drawing numbers when she gets k or more points.
+   Alice stops drawing numbers when she gets k or more points.
 
-Return the probability that Alice has n or fewer points.
+   Return the probability that Alice has n or fewer points.
 
-Answers within 10-5 of the actual answer are considered accepted.
+   Answers within 10-5 of the actual answer are considered accepted.
 
 
 
-Example 1:
+   Example 1:
 
 Input: n = 10, k = 1, maxPts = 10
 Output: 1.00000
@@ -41,16 +41,34 @@ Constraints:
 
 #include"head.h"
 class Solution {
-public:
-double new21Game(int n, int k, int maxPts) {
+	public:
+		double new21Game(int n, int k, int maxPts) {
+		if(k==0) return 1.0;
 
-}
+		vector<double> dp(n+1,0),sum(n+1,0);
+		dp[0]=sum[0]=1;
+
+		for(int i=1;i<=n;i++)
+		{
+			int l=max(0,i-maxPts),r=min(k-1,i-1);
+			if(l<=r)
+			{
+				if(l==0)
+					dp[i]=sum[r]/maxPts;
+				else
+					dp[i]=(sum[r]-sum[l-1])/maxPts;
+			}
+			sum[i]=sum[i-1]+dp[i];
+		}
+		return sum.back()-sum[k-1];
+		}
 };
 
-
-int main() 
- {
- 	 Solution s;
- //	 s.new21Game();
- 	 return 0;
- }
+int main()
+{
+	Solution s;
+	cout<<s.new21Game(10,1,10)<<endl;
+	cout<<s.new21Game(6,1,10)<<endl;
+	cout<<s.new21Game(21,17,10)<<endl;
+	return 0;
+}
