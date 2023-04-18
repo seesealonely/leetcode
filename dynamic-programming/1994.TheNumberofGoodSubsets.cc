1b48fa4198fa1db2d4e55173f682d1886a1e1da4
seesealonely <seesealonely@gmail.com>
2023-04-18 15:55:53 +0800

    dp + mask

---
+++ dynamic-programming/1994.TheNumberofGoodSubsets.cc
@@ -0,0 +1,113 @@
+/*
+   You are given an integer array nums. We call a subset of nums good if its product can be represented as a product of one or more distinct prime numbers.
+
+   For example, if nums = [1, 2, 3, 4]:
+   [2, 3], [1, 2, 3], and [1, 3] are good subsets with products 6 = 2*3, 6 = 2*3, and 3 = 3 respectively.
+   [1, 4] and [4] are not good subsets with products 4 = 2*2 and 4 = 2*2 respectively.
+
+   Return the number of different good subsets in nums modulo 109 + 7.
+
+   A subset of nums is any array that can be obtained by deleting some (possibly none or all) elements from nums. Two subsets are different if and only if the chosen indices to delete are different.
+
+
+
+   Example 1:
+
+Input: nums = [1,2,3,4]
+Output: 6
+Explanation: The good subsets are:
+- [1,2]: product is 2, which is the product of distinct prime 2.
+- [1,2,3]: product is 6, which is the product of distinct primes 2 and 3.
+- [1,3]: product is 3, which is the product of distinct prime 3.
+- [2]: product is 2, which is the product of distinct prime 2.
+- [2,3]: product is 6, which is the product of distinct primes 2 and 3.
+- [3]: product is 3, which is the product of distinct prime 3.
+
+Example 2:
+
+Input: nums = [4,2,3,15]
+Output: 5
+Explanation: The good subsets are:
+- [2]: product is 2, which is the product of distinct prime 2.
+- [2,3]: product is 6, which is the product of distinct primes 2 and 3.
+- [2,15]: product is 30, which is the product of distinct primes 2, 3, and 5.
+- [3]: product is 3, which is the product of distinct prime 3.
+- [15]: product is 15, which is the product of distinct primes 3 and 5.
+
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 30
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		int numberOfGoodSubsets(vector<int>& nums) {
+			typedef long long ll;
+			const int mod=1e9+7;
+			const vector<int> primes={2,3,5,7,11,13,17,19,23,29};
+			vector<int> cnt(31,0),dp(1024,0);
+			int n=nums.size();
+			for(int i=0;i<n;i++)
+				cnt[nums[i]]++;
+
+			dp[0]=1;
+			for(int x=2;x<=30;x++)
+			{
+				if(!cnt[x]) continue;
+
+				int mask=0,num=x;
+				bool success=true;
+
+				for(int i=0;i<10;i++)
+				{
+					int cntbit=0;
+					while(num%primes[i]==0)
+					{
+						++cntbit;
+						mask|=1<<i;
+						num/=primes[i];
+					}
+					if(cntbit>1)
+					{
+						success=false;
+						break;
+					}
+				}
+				if(!success)
+					continue;
+
+				for(int s=(1<<10)-1;s>=0;s--)
+				{
+					if(s&mask) continue;
+					dp[s|mask]=((ll)dp[s|mask]+(ll)dp[s]*cnt[x])%mod;
+				}
+			}
+
+			ll res=0;
+			for(int s=(1<<10)-1;s>=1;s--)
+				res=((ll)res+dp[s])%mod;
+			res;
+			for(int i=0;i<cnt[1];i++)
+				res=((ll)res*2)%mod;
+			return res;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<int> v={1,2,3,4};
+	cout<<s.numberOfGoodSubsets(v)<<endl;
+	v.clear();v={4,2,3,15};
+	cout<<s.numberOfGoodSubsets(v)<<endl;
+	v.clear();v={2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,14,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,21,21,21,21,21,21,21,21,21,21,21,22,22,22,22,22,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,29,30,30,30,30,30,30,30} ;
+	cout<<s.numberOfGoodSubsets(v)<<endl;
+	return 0;
+}
