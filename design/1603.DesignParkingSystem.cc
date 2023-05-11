65f8b2ec09fe3b67f7a7c6784e5bcabe416dd66e
seesealonely <seesealonely@gmail.com>
2023-05-11 08:10:04 +0800

    design

---
+++ design/1603.DesignParkingSystem.cc
@@ -0,0 +1,67 @@
+/*
+   Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+   Implement the ParkingSystem class:
+
+   ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+   bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+
+
+
+   Example 1:
+
+   Input
+   ["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+   [[1, 1, 0], [1], [2], [3], [1]]
+   Output
+   [null, true, true, false, false]
+
+   Explanation
+   ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+   parkingSystem.addCar(1); // return true because there is 1 available slot for a big car
+   parkingSystem.addCar(2); // return true because there is 1 available slot for a medium car
+   parkingSystem.addCar(3); // return false because there is no available slot for a small car
+   parkingSystem.addCar(1); // return false because there is no available slot for a big car. It is already occupied.
+
+
+
+Constraints:
+
+0 <= big, medium, small <= 1000
+carType is 1, 2, or 3
+At most 1000 calls will be made to addCar
+
+*/
+
+
+#include"head.h"
+class ParkingSystem {
+	public:
+		vector<int> park;
+		ParkingSystem(int big, int medium, int small) {
+		park.resize(4);
+		park[1]=big;
+		park[2]=medium;
+		park[3]=small;
+		}
+
+		bool addCar(int carType) {
+		if(park[carType]>0)
+		{
+			park[carType]--;
+			return true;
+		}
+			return false;
+		}
+};
+
+
+int main() 
+{
+	ParkingSystem park(1,1,0);
+	cout<<park.addCar(1)<<endl;
+	cout<<park.addCar(2)<<endl;
+	cout<<park.addCar(3)<<endl;
+	cout<<park.addCar(1)<<endl;
+	return 0;
+}
