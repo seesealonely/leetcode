db58d857c019cba50d089999f4c530cf6ef0c67c
seesealonely <seesealonely@gmail.com>
2023-08-17 15:55:45 +0800

    enum

---
+++ enum/2815.MaxPairSuminanArray.cc
@@ -0,0 +1,70 @@
+/*
+
+   You are given a 0-indexed integer array nums. You have to find the maximum sum of a pair of numbers from nums such that the maximum digit in both numbers are equal.
+
+   Return the maximum sum or -1 if no such pair exists.
+
+
+
+   Example 1:
+
+Input: nums = [51,71,17,24,42]
+Output: 88
+Explanation:
+For i = 1 and j = 2, nums[i] and nums[j] have equal maximum digits with a pair sum of 71 + 17 = 88.
+For i = 3 and j = 4, nums[i] and nums[j] have equal maximum digits with a pair sum of 24 + 42 = 66.
+It can be shown that there are no other pairs with equal maximum digits, so the answer is 88.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: -1
+Explanation: No pair exists in nums with equal maximum digits.
+
+
+
+Constraints:
+
+2 <= nums.length <= 100
+1 <= nums[i] <= 104
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		int maxSum(vector<int>& nums) {
+		const int n=nums.size();
+		vector<int> mx(n);
+		for(int i=0;i<n;i++)
+		{
+			int x=nums[i],ma=0;
+			while(x)
+			{
+				ma=max(ma,x%10);
+				x/=10;
+			}
+			mx[i]=ma;
+		}
+
+		int res=-1;
+		for(int i=0;i<n;i++)
+			for(int j=i+1;j<n;j++)
+				if(mx[i]==mx[j])
+					res=max(res,nums[i]+nums[j]);
+
+		return res;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<int> nums = {51,71,17,24,42};
+	cout<<s.maxSum(nums)<<endl;
+	nums.clear();nums={1,2,3,4};
+	cout<<s.maxSum(nums)<<endl;
+	return 0;
+}
