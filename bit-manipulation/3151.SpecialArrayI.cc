24352c3a73fcfe84a7d8418f41c658d424d9425f
seesealonely <seesealonely@gmail.com>
2024-05-21 04:18:44 +0800

    bit operation

---
+++ bit-manipulation/3151.SpecialArrayI.cc
@@ -0,0 +1,71 @@
+/*
+   An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+
+   You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
+
+
+   Example 1:
+
+Input: nums = [1]
+
+Output: true
+
+Explanation:
+
+There is only one element. So the answer is true.
+
+Example 2:
+
+Input: nums = [2,1,4]
+
+Output: true
+
+Explanation:
+
+There is only two pairs: (2,1) and (1,4), and both of them contain numbers with different parity. So the answer is true.
+
+Example 3:
+
+Input: nums = [4,3,1,6]
+
+Output: false
+
+Explanation:
+
+nums[1] and nums[2] are both odd. So the answer is false.
+
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		bool isArraySpecial(vector<int>& nums) {
+			const int n=nums.size();
+			for(int i=0;i<n-1;i++)
+				if(((nums[i]^nums[i+1])&1)^1)
+					return false;
+			return true;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<int> nums={1};
+	cout<<s.isArraySpecial(nums)<<endl;
+	nums.clear(); nums={2,1,4};
+	cout<<s.isArraySpecial(nums)<<endl;
+	nums.clear();nums={4,3,1,6};
+	cout<<s.isArraySpecial(nums)<<endl;
+	return 0;
+}
