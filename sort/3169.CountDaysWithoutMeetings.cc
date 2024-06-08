2e9b573b25e1cf288226025a222b43f7eba81b42
seesealonely <seesealonely@gmail.com>
2024-06-08 10:04:23 +0800

    sort

---
+++ sort/3169.CountDaysWithoutMeetings.cc
@@ -0,0 +1,85 @@
+/*
+   You are given a positive integer days representing the total number of days an employee is available for work (starting from day 1). You are also given a 2D array meetings of size n where, meetings[i] = [start_i, end_i] represents the starting and ending days of meeting i (inclusive).
+
+   Return the count of days when the employee is available for work but no meetings are scheduled.
+
+Note: The meetings may overlap.
+
+
+
+Example 1:
+
+Input: days = 10, meetings = [[5,7],[1,3],[9,10]]
+
+Output: 2
+
+Explanation:
+
+There is no meeting scheduled on the 4th and 8th days.
+
+Example 2:
+
+Input: days = 5, meetings = [[2,4],[1,3]]
+
+Output: 1
+
+Explanation:
+
+There is no meeting scheduled on the 5th day.
+
+Example 3:
+
+Input: days = 6, meetings = [[1,6]]
+
+Output: 0
+
+Explanation:
+
+Meetings are scheduled for all working days.
+
+
+
+Constraints:
+
+1 <= days <= 109
+1 <= meetings.length <= 105
+meetings[i].length == 2
+1 <= meetings[i][0] <= meetings[i][1] <= days
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		int countDays(int days, vector<vector<int>>& meetings) {
+			const int n=meetings.size();
+			sort(meetings.begin(),meetings.end());
+
+			int s=meetings[0][0],e=meetings[0][1];
+			for(int i=1;i<n;i++)
+			{
+				if(e<meetings[i][0])
+				{
+					days-=e-s+1;
+					s=meetings[i][0];
+				}
+				e=max(e,meetings[i][1]);
+			}
+			days-=e-s+1;
+			return days;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<vector<int>>  meetings = {{5,7},{1,3},{9,10}};
+	cout<<s.countDays(10,meetings)<<endl;
+	meetings.clear();meetings={{2,4},{1,3}};
+	cout<<s.countDays(5,meetings)<<endl;
+	meetings.clear();meetings={{1,6}};
+	cout<<s.countDays(6,meetings)<<endl;
+	return 0;
+}
