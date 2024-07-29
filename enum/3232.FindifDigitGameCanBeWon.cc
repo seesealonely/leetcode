fad411a2736795d45d9277deb18831a303b4c7ef
seesealonely <seesealonely@gmail.com>
2024-07-30 07:34:48 +0800

    enum

---
+++ enum/3232.FindifDigitGameCanBeWon.cc
@@ -0,0 +1,75 @@
+/*
+   You are given an array of positive integers nums.
+
+   Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
+
+   Return true if Alice can win this game, otherwise, return false.
+
+
+
+   Example 1:
+
+Input: nums = [1,2,3,4,10]
+
+Output: false
+
+Explanation:
+
+Alice cannot win by choosing either single-digit or double-digit numbers.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5,14]
+
+Output: true
+
+Explanation:
+
+Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+Example 3:
+
+Input: nums = [5,5,5,25]
+
+Output: true
+
+Explanation:
+
+Alice can win by choosing double-digit numbers which have a sum equal to 25.
+
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 99
+
+*/
+
+
+#include"head.h"
+class Solution {
+	public:
+		bool canAliceWin(vector<int>& nums) {
+		int res=0,n=nums.size();
+		for(int i=0;i<n;i++)
+			if(nums[i]<10)
+				res+=nums[i];
+			else
+				res-=nums[i];
+		return res!=0;
+		}
+};
+
+
+int main() 
+{
+	Solution s;
+	vector<int> nums = {1,2,3,4,10};
+	cout<<s.canAliceWin(nums)<<endl;
+	nums.clear();nums={1,2,3,4,5,14};
+	cout<<s.canAliceWin(nums)<<endl;
+	nums.clear();nums={5,5,5,25};
+	cout<<s.canAliceWin(nums)<<endl;
+	return 0;
+}
